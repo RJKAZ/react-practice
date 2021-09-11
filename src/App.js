03973f43740816deb70bc35e50c186eecc6cd52c
RJ <rjkaz@comcast.net>
2021-09-10 23:57:57 -0400

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Fragment } from 'react/cjs/react.production.min';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Loop from './components/Loop';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello New Repo</h1>
+    <Fragment>
+      <BrowserRouter>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/loop" component={Loop}/>
+      </Switch>
+      </BrowserRouter>
+
+    </Fragment>
+   
   );
 }
 
---
+++ src/components/Loop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loop = () => {
+    return (
+        <div>
+            <h1>Loop exercise</h1>
+        </div>
+    )
+}
+
+export default Loop
